08af771fa0e0eabf7884943d334d29f045124f4d
rohartbastien@gmail.com <rohartbastien@gmail.com@a06aa917-ab39-a05c-3315-d6acf4be9e0d>
2011-11-20 15:31:44 +0000

    graphique

--- trunk/src/PlateauGraphique.java
+++ trunk/src/PlateauGraphique.java
@@ -11,6 +11,11 @@ public class PlateauGraphique extends JComponent {
     @Override
     public void paintComponent(Graphics g) {
         Graphics2D drawable = (Graphics2D) g;
-        
+
+        int width = getWidth();
+        int height = getHeight();
+
+        drawable.setColor(Color.black);
+        drawable.drawRect(0, 0, 300, 300);
     }
 }
