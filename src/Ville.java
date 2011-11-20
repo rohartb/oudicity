0d83aa8473db073cfdd93e160a74339c57793451
clercmargo@gmail.com <clercmargo@gmail.com@a06aa917-ab39-a05c-3315-d6acf4be9e0d>
2011-11-20 17:01:27 +0000

    "Temps + debut impot"

--- src/Ville.java
+++ src/Ville.java
@@ -1,5 +1,15 @@
 public class Ville {
     String nomVille;
+    String nomMaire;
     int nbHabitant;
+    int argent;
+    int indiceAttraction;
     
+    Ville(String v, String m){
+        nomVille = v;
+        nomMaire = m;
+        argent = 20000;
+        nbHabitant = 0;
+        indiceAttraction = 50;
+    }   
 }
