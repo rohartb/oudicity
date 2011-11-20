2f128297cde8efe80554ca28157f2bf5962d6ab6
rohartbastien@gmail.com <rohartbastien@gmail.com@a06aa917-ab39-a05c-3315-d6acf4be9e0d>
2011-11-20 18:15:03 +0000

    float

--- trunk/src/PlateauGraphique.java
+++ trunk/src/PlateauGraphique.java
@@ -1,32 +1,34 @@
 import java.awt.*;
+import java.awt.geom.Line2D;
 import javax.swing.*;
 
 public class PlateauGraphique extends JComponent {
     Ville ville;
     int N=60;
-    
+
     PlateauGraphique(Ville v){
         this.ville = v;
     }
- 
-    @Override
-    public void paintComponent(Graphics g) {
+
+     public void paintComponent(Graphics g) {
         Graphics2D drawable = (Graphics2D) g;
 
         int width = getSize().width;
         int height = getSize().height;
-        int xM = (width*20)/100;
-        int xCase = (width-xM)/N;
+        float xM = (width*20)/100;
+        float xCase = (width-xM)/N;
        
 
         drawable.setPaint(Color.white);
-        drawable.fillRect(xM, 0, width, height);
+        drawable.fillRect((int) xM, 0, width, height);
         drawable.setPaint(Color.black);
 
         for(int i=0; i<=N; i++){
-            drawable.drawLine(i*xCase+xM, 0, i*xCase+xM, height);
-            drawable.drawLine(xM, i*(height/N), width, i*(height/N));
+            drawable.draw(new Line2D.Float(i*xCase+xM, 0, i*xCase+xM, height));
+            drawable.draw(new Line2D.Float(xM, i*(height/N), width, i*(height/N)));
         }
 
     }
 }
+    
+ 
