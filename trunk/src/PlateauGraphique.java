318dbe02fc2ed8a0c82558e2cb70964059cae518
rohartbastien@gmail.com <rohartbastien@gmail.com@a06aa917-ab39-a05c-3315-d6acf4be9e0d>
2011-11-20 17:02:20 +0000

    Graphique

--- trunk/src/PlateauGraphique.java
+++ trunk/src/PlateauGraphique.java
@@ -3,19 +3,30 @@ import javax.swing.*;
 
 public class PlateauGraphique extends JComponent {
     AireDeJeu aire;
+    int N=60;
     
     PlateauGraphique(AireDeJeu a){
-        aire = a;
+        this.aire = a;
     }
  
     @Override
     public void paintComponent(Graphics g) {
         Graphics2D drawable = (Graphics2D) g;
 
-        int width = getWidth();
-        int height = getHeight();
+        int width = getSize().width;
+        int height = getSize().height;
+        int xM = (width*20)/100;
 
-        drawable.setColor(Color.black);
-        drawable.drawRect(0, 0, 300, 300);
+       
+
+        drawable.setPaint(Color.white);
+        drawable.fillRect(0, 0, width, height);
+        drawable.setPaint(Color.black);
+
+        for(int i=0; i<=N; i++){
+            drawable.drawLine(i*((width-xM)/N)+xM, 0, i*((width-xM)/N), height);
+            drawable.drawLine(xM, i*(height/N), width, i*(height/N));
+        }
+
     }
 }
