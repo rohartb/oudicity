579d11a9ae8103fb92a39c4f9a0ac54b72524802
clercmargo@gmail.com <clercmargo@gmail.com@a06aa917-ab39-a05c-3315-d6acf4be9e0d>
2011-12-03 10:38:28 +0000

    BarreLaterale

--- src/BarreLaterale.java
+++ src/BarreLaterale.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.*;
 import javax.swing.*;
 
@@ -5,22 +6,51 @@ import javax.swing.*;
 public class BarreLaterale extends JPanel implements Observer {
     Ville ville;
     Temps t;
+    JPanel info;
+    JPanel batiments;
+    JPanel conseils;
     JLabel nomMaire;
     JLabel nbHab;
     JLabel date;
-    JButton boutonL = new JButton("Logement");
+    JButton boutonL;
    
     int annee = 1970;
     int jour = 1;
     int mois = 1;
     int nbHabitant = 0;
-    int argent = 20000;
+    int argent = 20000;   
     
     public BarreLaterale(Ville v,Temps t){
         ville = v;
         this.t = t;
         t.c.addObserver(this);
         ville.addObserver(this);
+        
+        
+        this.setLayout(new BorderLayout());
+        this.setPreferredSize(new Dimension(200,700));
+        this.setLayout(new BorderLayout());
+        
+        //Panel au NORD contenant les informations
+        info = new JPanel();
+        info.setBackground(new Color(153,204,255));
+        info.setPreferredSize(new Dimension(200,200));
+        info.setLayout(new BoxLayout(info,BoxLayout.Y_AXIS));
+        
+        // Ajout des Labels
+        nomMaire = new JLabel("Maire " + ville.nomMaire);
+        nbHab = new JLabel("" +ville.nbHabitant);
+        date = new JLabel(t.c.afficherDate());
+       
+        info.add(nomMaire);
+        info.add(nbHab);
+        
+        boutonL = new JButton("Logement");
+        
+        //this.add(nomMaire);
+        //this.add(nbHab);
+        //this.add(date);
+        this.add(info,BorderLayout.NORTH);
     }
   
     
