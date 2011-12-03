cc79a93a82c29bacae52378c16b97e676ce33a85
rohartbastien@gmail.com <rohartbastien@gmail.com@a06aa917-ab39-a05c-3315-d6acf4be9e0d>
2011-12-03 14:09:59 +0000

    barre latérale écouteur

--- src/EcouteurDeBarreLateral.java
+++ src/EcouteurDeBarreLateral.java
@@ -1,7 +1,6 @@
 
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 /*
  * To change this template, choose Tools | Templates
@@ -12,19 +11,71 @@ import java.awt.event.ActionListener;
  *
  * @author bastien
  */
-class EcouteurDeBarreLateral implements ActionListener{
+class EcouteurDeBarreLateral implements ActionListener,
+                                         MouseListener,
+                                  MouseMotionListener {
+    
     Image maison, immeuble, mairie, hopital;
     PlateauGraphique graphe;
 
     public EcouteurDeBarreLateral(PlateauGraphique g){
         graphe = g;
-        //maison = graphe.getToolkit().getImage("Image/maison.jpg");
-        //immeuble = graphe.getToolkit().getImage("Image/immeuble.gif");
-        //mairie = graphe.getToolkit().getImage("Image/mairie.png");
-        //hopital = graphe.getToolkit().getImage("Image/hopital.jpg");
+        maison = graphe.getToolkit().getImage("Image/maison.gif");
+        immeuble = graphe.getToolkit().getImage("Image/immeuble.gif");
+        mairie = graphe.getToolkit().getImage("Image/mairie.gif");
+        hopital = graphe.getToolkit().getImage("Image/hospital.gif");
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        int xcase = (graphe.width/graphe.N);
+        int ycase = (graphe.height/graphe.N);
+
+        if (e.getActionCommand().equals("logement")){
+            //graphe.getGraphics().drawImage(maison,i*(width/N), j*(height/N),width/N,
+              //          height/N, null);
+        }else if (e.getActionCommand().equals("industrie")){
+
+        }else if (e.getActionCommand().equals("commerce")){
+
+        }else if (e.getActionCommand().equals("ferme")){
+
+        }else if (e.getActionCommand().equals("route")){
+            
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
