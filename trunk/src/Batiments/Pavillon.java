14d0cf23bc6a9a3ae6565fc96c3b1c10f8f9bb9d
clercmargo@gmail.com <clercmargo@gmail.com@a06aa917-ab39-a05c-3315-d6acf4be9e0d>
2011-12-04 21:07:43 +0000

    ça va mieux

--- trunk/src/Batiments/Pavillon.java
+++ trunk/src/Batiments/Pavillon.java
@@ -10,7 +10,7 @@ public class Pavillon extends Logement {
         this.prixDestr=5;
         this.taille=1;
         this.type="pavillon";
-        this.nb_habitant_MAX=100;
+        this.nb_habitant_MAX=10;
         this.nb_habitant=0;
     }
     
