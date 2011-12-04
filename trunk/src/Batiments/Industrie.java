c11040c74b5bb808a39c2d81d741d7c4fc396830
sophie.penot07@gmail.com <sophie.penot07@gmail.com@a06aa917-ab39-a05c-3315-d6acf4be9e0d>
2011-12-04 18:15:47 +0000

    modif  ferme

--- trunk/src/Batiments/Industrie.java
+++ trunk/src/Batiments/Industrie.java
@@ -1,6 +1,9 @@
 package Batiments;
 
 public class Industrie extends BatimentArgent{
+    private int besoin;
+    private int production;
+    private int bien;
     public Industrie(){
         this.setCoutEntretien(10);
         this.setNb_employe_MAX(20);
@@ -9,6 +12,21 @@ public class Industrie extends BatimentArgent{
         this.setPrixDestr(5);
         this.setTaille(1);
         this.setType("industrie");
-        this.setBesoin(100);        
+        this.besoin=100; 
+        this.bien=this.besoin/10;
     }
+
+    public int getBesoin() {
+        return besoin;
+    }
+
+    public int getBien() {
+        return bien;
+    }
+
+    public void setBesoin(int besoin) {
+        this.besoin = besoin;
+        this.bien = besoin/10;
+    }
+    
 }
