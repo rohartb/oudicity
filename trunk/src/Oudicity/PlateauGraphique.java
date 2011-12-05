1bf4761f496d8ba61cc20440691add463d91503d
rohartbastien@gmail.com <rohartbastien@gmail.com@a06aa917-ab39-a05c-3315-d6acf4be9e0d>
2011-12-05 13:38:13 +0000

    ajout de tous les popups

--- trunk/src/Oudicity/PlateauGraphique.java
+++ trunk/src/Oudicity/PlateauGraphique.java
@@ -51,31 +51,105 @@ public class PlateauGraphique extends JComponent {
                     drawable.setPaint(new Color(153,255,51));
                     drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
                                      (int)height/N);
-                    drawable.setPaint(Color.black);
-                }else if(this.p.plateau[i][j].getType().equals("pavillon")){
+                }
+
+                //paint pour les logements
+                else if(this.p.plateau[i][j].getType().equals("pavillon")){
                     drawable.setPaint(new Color(255,51,51));
                     drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
                                      (int)height/N);
-                    drawable.setPaint(Color.black);
-                }else if(this.p.plateau[i][j].getType().equals("industrie")){
+                }else if(this.p.plateau[i][j].getType().equals("hlm")){
+                    drawable.setPaint(new Color(0,0,0));
+                    drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
+                                     (int)height/N);
+                }else if(this.p.plateau[i][j].getType().equals("building")){
+                    drawable.setPaint(new Color(0,0,0));
+                    drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
+                                     (int)height/N);
+                }
+
+
+                // paint pour les batiments argents
+                else if(this.p.plateau[i][j].getType().equals("industrie")){
                     drawable.setPaint(new Color(51,153,153));
                     drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
                                      (int)height/N);
-                    drawable.setPaint(Color.black);
                 }else if(this.p.plateau[i][j].getType().equals("ferme")){
                     drawable.setPaint(new Color(139,69,19));
                     drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
                                      (int)height/N);
-                    drawable.setPaint(Color.black);
+                }else if(this.p.plateau[i][j].getType().equals("commerce")){
+                    drawable.setPaint(new Color(0,0,0));
+                    drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
+                                     (int)height/N);
                 }
+
+                // paint pour les infrastructures
                 else if(this.p.plateau[i][j].getType().equals("route")){
                     drawable.setPaint(new Color(51,51,51));
                     drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
                                      (int)height/N);
-                    drawable.setPaint(Color.black);
+                }
+
+
+                // paint pour les loisirs
+                else if(this.p.plateau[i][j].getType().equals("bar")){
+                    drawable.setPaint(new Color(0,0,0));
+                    drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
+                                     (int)height/N);
+                }else if(this.p.plateau[i][j].getType().equals("cinema")){
+                    drawable.setPaint(new Color(0,0,0));
+                    drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
+                                     (int)height/N);
+                }else if(this.p.plateau[i][j].getType().equals("parc")){
+                    drawable.setPaint(new Color(0,0,0));
+                    drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
+                                     (int)height/N);
+                }else if(this.p.plateau[i][j].getType().equals("restaurant")){
+                    drawable.setPaint(new Color(0,0,0));
+                    drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
+                                     (int)height/N);
+                }else if(this.p.plateau[i][j].getType().equals("stade")){
+                    drawable.setPaint(new Color(0,0,0));
+                    drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
+                                     (int)height/N);
+                }else if(this.p.plateau[i][j].getType().equals("zoo")){
+                    drawable.setPaint(new Color(0,0,0));
+                    drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
+                                     (int)height/N);
+                }
+
+
+                // paint pour les services publiques
+                else if(this.p.plateau[i][j].getType().equals("commissariat")){
+                    drawable.setPaint(new Color(0,0,0));
+                    drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
+                                     (int)height/N);
+                }else if(this.p.plateau[i][j].getType().equals("decheterie")){
+                    drawable.setPaint(new Color(0,0,0));
+                    drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
+                                     (int)height/N);
+                }else if(this.p.plateau[i][j].getType().equals("ecole")){
+                    drawable.setPaint(new Color(0,0,0));
+                    drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
+                                     (int)height/N);
+                }else if(this.p.plateau[i][j].getType().equals("hopital")){
+                    drawable.setPaint(new Color(0,0,0));
+                    drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
+                                     (int)height/N);
+                }else if(this.p.plateau[i][j].getType().equals("hotel")){
+                    drawable.setPaint(new Color(0,0,0));
+                    drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
+                                     (int)height/N);
+                }else if(this.p.plateau[i][j].getType().equals("pompier")){
+                    drawable.setPaint(new Color(45,112,24));
+                    drawable.fillRect((int)(i*(width/N)),(int) (j*(height/N)), (int)width/N,
+                                     (int)height/N);
                 }
             }
         }
+
+        drawable.setPaint(Color.black);
         for(int i=0; i<=N; i++){
             drawable.draw(new Line2D.Float(i*xCase, 0, i*xCase, height));
             drawable.draw(new Line2D.Float(0, i*(height/N), width,
